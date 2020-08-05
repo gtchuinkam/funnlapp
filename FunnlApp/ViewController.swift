7fcfdec5f4d67ce880f75d7c302fa4e253062cf3
GTchuinkam <gtchuinkamtchuinkam@colgate.edu>
2020-08-05 17:37:45 -0400

    Initial Commit

---
+++ FunnlApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunnlApp
+//
+//  Created by Germain Brachelt Tchuinkam Tchuinkam on 8/5/20.
+//  Copyright © 2020 Germain Brachelt Tchuinkam Tchuinkam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
